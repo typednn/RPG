f6120e902641e72f4564ff6f1c1d514f238b3c49
hzaskywalker <z2huang@eng.ucsd.edu>
2022-10-04 16:22:13 -0700

    added several generative model

--- rpg/tester/test_ppo_swimmer.py
+++ rpg/tester/test_ppo_swimmer.py
@@ -7,4 +7,5 @@ from rpg.ppo import train_ppo
 N = 10
 env = GymVecEnv('Swimmer-v2', N)
 
+# swimmer must clip the done in the end.
 train_ppo.parse(env, steps=2000, obs_norm=True, actor=dict(head=dict(linear=False)), gae=dict(correct_gae=False, gamma=0.999, use_env_done=False), batch_size=2000) # use tanh
