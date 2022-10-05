04ee2139aad9416e87bfb76773b529c69c5cf554
hzaskywalker <z2huang@eng.ucsd.edu>
2022-10-05 13:28:17 -0700

    update

--- rpg/tester/test_ppo_cheetah.py
+++ rpg/tester/test_ppo_cheetah.py
@@ -7,4 +7,4 @@ from rpg.ppo import train_ppo
 N = 10
 env = GymVecEnv('HalfCheetah-v3', N)
 
-train_ppo.parse(env, steps=5000, obs_norm=True, actor=dict(head=dict(linear=False, std_scale=0.3)), gae=dict(correct_gae=True), batch_size=2000) # use tanh
+train_ppo.parse(env, steps=2000, obs_norm=True, actor=dict(head=dict(linear=False, std_scale=0.36, std_mode='statewise')), gae=dict(correct_gae=True), batch_size=2000, ppo=dict(learning_epoch=2)) # use tanh
