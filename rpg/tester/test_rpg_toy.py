42113390fc958206e6f9c157ee4d149081b054c8
hzaskywalker <z2huang@eng.ucsd.edu>
2022-10-05 21:13:55 -0700

    run rpg

--- rpg/tester/test_rpg_toy.py
+++ rpg/tester/test_rpg_toy.py
@@ -8,4 +8,4 @@ N = 100
 env = TorchEnv('TripleMove', N)
 hidden_space  = Discrete(5)
 
-train_rpg.parse(env, hidden_space, steps=env.max_time_steps, hidden_head=dict(epsilon=0.), reward_norm=True, relbo=dict(prior=0.01, ent_a=0.1, ent_z=10.), hooks=dict(save_traj=dict(n_epoch=1)))
+train_rpg.parse(env, hidden_space, steps=env.max_time_steps, hidden_head=dict(epsilon=0.), reward_norm=True, relbo=dict(prior=0.01, ent_a=0.1, ent_z=0.1), hooks=dict(save_traj=dict(n_epoch=1)))
