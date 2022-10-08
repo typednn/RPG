376777e1fdccb24e896810669485cea467b0056a
hzaskywalker <z2huang@eng.ucsd.edu>
2022-10-08 14:44:36 -0700

    update

--- rpg/tester/test_rnd_rpg.py
+++ rpg/tester/test_rnd_rpg.py
@@ -7,7 +7,7 @@ from nn.space import Discrete
 
 # N = 1
 N = 256
-env = TorchEnv('LargeMaze', N, reward=False)
+env = TorchEnv('SmallMaze', N, reward=False)
 
 hidden_space  = Discrete(4)
 
@@ -15,20 +15,19 @@ train_rpg.parse(
     env, 
     hidden_space,
 
-    steps=200, obs_norm=False, reward_norm=True,
+    steps=40, obs_norm=False, reward_norm=True,
 
     hidden_head=dict(epsilon=0.),
 
-    K=4,
     actor=dict(
         head=dict(linear=False, std_scale=0.2, std_mode='fix_no_grad')
     ),
-    relbo=dict(prior=0.0, mutual_info=0.1, ent_a=0.0, ent_z=0.),
+    relbo=dict(prior=0.0, mutual_info=0.01, ent_a=0.0, ent_z=0.),
 
     gae=dict(lmbda=0.97),
     batch_size=2000,
     ppo=dict(learning_epoch=5),
-    ppo_higher=dict(learning_epoch=0),
+    ppo_higher=dict(learning_epoch=5, actor_optim=dict(entropy_coef=10.)),
 
     hooks=dict(
         #save_model=dict(n_epoch=10),
@@ -37,5 +36,5 @@ train_rpg.parse(
         save_traj=dict(n_epoch=1, save_gif_epochs=10),
     ),
     rnd=dict(learning_epoch=2), # no rnd
-    path='tmp/rnd/rpg_large',
+    path='tmp/rnd/rpg2',
 ) # use tanh
