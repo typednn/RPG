18b7e0a6d57a7856738c7826781e8064496fa030
hzaskywalker <z2huang@eng.ucsd.edu>
2022-10-15 15:34:56 -0700

    update sac..

--- rpg/tester/test_model_based.py
+++ rpg/tester/test_model_based.py
@@ -7,8 +7,9 @@ from rpg.rpgm import Trainer
 
 # N = 1
 #N = 1
-# env = GymVecEnv('HalfCheetah-v3', 10, ignore_truncated_done=True)
-env = TorchEnv('SmallMaze', n=100, ignore_truncated=True, reward=True)
+# env = TorchEnv('SmallMaze', n=100, ignore_truncated=True, reward=True)
 
-trainer = Trainer.parse(env, update_step=10, buffer=dict(priority=False), head=dict(squash=True), hooks=dict(save_traj=dict(n_epoch=2, save_gif_epochs=10)))
+# trainer = Trainer.parse(env, update_step=10, buffer=dict(priority=False), head=dict(squash=True), hooks=dict(save_traj=dict(n_epoch=2, save_gif_epochs=10)))
+env = GymVecEnv('HalfCheetah-v3', 10, ignore_truncated_done=True)
+trainer = Trainer.parse(env, update_step=200, buffer=dict(priority=False), head=dict(std_mode='statewise', std_scale=1., squash=True), entropy_coef=1., entropy_target=-6)
 trainer.run_rpgm()
