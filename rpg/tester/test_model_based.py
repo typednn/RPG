89d93eae857c431109393e5cad2de436f67d1f30
hzaskywalker <z2huang@eng.ucsd.edu>
2022-10-19 15:24:23 -0700

    update

--- rpg/tester/test_model_based.py
+++ rpg/tester/test_model_based.py
@@ -12,7 +12,7 @@ from rpg.rpgm import Trainer
 # trainer = Trainer.parse(env, update_step=10, buffer=dict(priority=False), head=dict(squash=True), hooks=dict(save_traj=dict(n_epoch=2, save_gif_epochs=10)))
 env = GymVecEnv('HalfCheetah-v3', 1, ignore_truncated_done=True)
 # trainer = Trainer.parse(env, max_update_step=1000, buffer=dict(priority=False), head=dict(std_mode='statewise', std_scale=1., squash=True), entropy_coef=1., entropy_target=-6, actor_optim=dict(max_grad_norm=1.), update_train_step=0) # do not know if we need max_grad_norm
-trainer = Trainer.parse(env, max_update_step=100, buffer=dict(priority=False), head=dict(std_mode='statewise', std_scale=1., squash=True), entropy_coef=1., entropy_target=-6, actor_optim=dict(max_grad_norm=1.), update_train_step=0, hooks=dict(evaluate_pi=dict())) # do not know if we need max_grad_norm
+trainer = Trainer.parse(env, max_update_step=1000, buffer=dict(priority=False), head=dict(std_mode='statewise', std_scale=1., squash=True), entropy_coef=1., entropy_target=-6, actor_optim=dict(max_grad_norm=1.), update_train_step=0, hooks=dict(evaluate_pi=dict())) # do not know if we need max_grad_norm
 # trainer = Trainer.parse(env, update_step=200, buffer=dict(priority=False), head=dict(std_mode='statewise', std_scale=1., squash=True), entropy_coef=1., entropy_target=-6, actor_optim=dict(max_grad_norm=1.), weights=dict(state=100., prefix=0.5, value=0.5)) # do not know if we need max_grad_norm
 # trainer = Trainer.parse(env, update_step=1000, buffer=dict(priority=False), head=dict(std_mode='statewise', std_scale=1., squash=True), entropy_coef=1., entropy_target=-6, actor_optim=dict(max_grad_norm=1.), critic_weight=1.) # do not know if we need max_grad_norm
 trainer.run_rpgm()
