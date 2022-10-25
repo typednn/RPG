1c22ec6dc346d8866c256d5fdd74a73b5bb63ca6
hzaskywalker <z2huang@eng.ucsd.edu>
2022-10-25 16:56:01 -0700

    xx

---
+++ rpg/worldmodel.py
@@ -0,0 +1,160 @@
+"""
+# model-based verison RPG
+s      a_1    a_2    ...
+|      |      |
+h_0 -> h_1 -> h_2 -> h_3 -> h_4
+        |      |      |      |   
+        o_1    o_2    o_3    o_4
+        / |  
+    s1 r1    
+    |
+    v1
+"""
+import torch
+from tools.nn_base import Network
+from tools.utils import totensor
+from .utils import done_rewards_values, lmbda_decay_weight, compute_value_prefix
+
+
+class GeneralizedQ(Network):
+    def __init__(
+        self,
+        enc_s, enc_a, enc_z,
+        pi_a, pi_z,
+
+        init_h, dynamic_fn,
+        state_dec, value_prefix, value_fn,
+
+        done_fn,
+
+        cfg=None,
+        gamma=0.99,
+        lmbda=0.97,
+
+        lmbda_last=False,
+
+
+        horizon=1,
+    ) -> None:
+        super().__init__()
+
+        self.pi_a, self.pi_z = pi_a, pi_z
+        self.enc_s, self.enc_z, self.enc_a = enc_s, enc_z, enc_a
+        assert self.pi_z is None
+
+        self.init_h = init_h
+        self.dynamic_fn: torch.nn.GRU = dynamic_fn
+
+        self.state_dec = state_dec
+        self.value_prefix = value_prefix
+        self.value_fn = value_fn
+
+        self.done_fn = done_fn
+            
+
+        v_nets = [enc_s, enc_z, value_fn]
+        d_nets = [enc_a, init_h, dynamic_fn, state_dec, value_prefix]
+        self.dynamics = torch.nn.ModuleList(d_nets + v_nets)
+
+        weights = lmbda_decay_weight(lmbda, horizon, lmbda_last=lmbda_last)
+        self._weights = torch.nn.Parameter(torch.log(weights), requires_grad=True)
+
+    @property
+    def weights(self):
+        return torch.softmax(self._weights, 0)
+
+
+    def policy(self, obs, z):
+        obs = totensor(obs, self.device)
+        s = self.enc_s(obs)
+        return self.pi_a(s, self.enc_z(z))
+
+    def value(self, obs, z):
+        obs = totensor(obs, self.device)
+        s = self.enc_s(obs)
+        return self.value_fn(s, self.enc_z(z))
+
+
+    def inference(self, obs, z, step, z_seq=None, a_seq=None, extra_rewards_fn=None):
+        sample_z = (z_seq is None)
+        if sample_z:
+            z_seq, logp_z = [], []
+        sample_a = (a_seq is None)
+        if sample_a:
+            a_seq, logp_a = [], []
+
+        hidden = []
+        states = []
+
+        s = self.enc_s(obs)
+        z_embed = self.enc_z(z)
+
+        # for dynamics part ..
+        h = self.init_h(s)
+        h = h.reshape(len(s), self.dynamic_fn.layer, -1).permute(1, 0, 2).contiguous() # GRU of layer 2
+        for idx in range(step):
+            assert self.pi_z is None, "this means that we will not update z, anyway.."
+
+            if len(z_seq) <= idx:
+                z, logp = self.pi_z(s, z_embed).sample()
+                logp_z.append(logp[..., None])
+                z_seq.append(z)
+
+            if len(a_seq) <= idx:
+                a, logp = self.pi_a(s, z_embed).rsample()
+                logp_a.append(logp[..., None])
+                a_seq.append(a)
+
+            z_embed = self.enc_z(z_seq[idx])
+            a_embed = self.enc_a(a_seq[idx])
+            o, h = self.dynamic_fn(a_embed[None, :].expand_as(h), h)
+            o = o[-1]
+            s = self.state_dec(o) # predict the next hidden state ..
+
+            hidden.append(o)
+            states.append(s)
+
+        stack = torch.stack
+        hidden = stack[hidden]
+        states = stack(states)
+        out = dict(hidden=stack(hidden), states=stack(states))
+
+        if sample_a:
+            out['a'] = stack(a_seq)
+            out['logp_a'] = stack(logp_a)
+
+        if sample_z:
+            z_seq = out['z'] = stack(z_seq)
+            out['logp_z'] = stack(logp_z)
+
+        out['value_prefix'] = value_prefix
+        value_prefix = self.value_prefix(hidden)
+
+
+        prefix = value_prefix
+        if extra_rewards_fn is not None:
+            extra_rewards, infos = extra_rewards_fn(**locals())
+            out.update(infos)
+            prefix = prefix + compute_value_prefix(extra_rewards, self._cfg.gamma)
+
+        values = self.value_fn(states, self.enc_z(z_seq)) # use the invariant of the value function ..
+
+        out['dones'] = dones = torch.sigmoid(self.done_fn(hidden)) if self.done_fn is not None else None
+        expected_values, expected_prefix = done_rewards_values(values, prefix, dones)
+
+        gamma = 1
+        vpreds = []
+        for i in range(len(hidden)):
+            vpred = (expected_prefix[i] + expected_values[i] * gamma * self._cfg.gamma)
+            vpreds.append(vpred)
+            gamma *= self._cfg.gamma
+
+        vpreds = stack(vpreds)
+        out['value'] = (vpreds * self.weights[:, None, None]).sum(axis=0)
+        return out
+
+
+    def entropy_rewards(self, logp_a, alpha=0.):
+        entropy_term = -logp_a
+        entropy = entropy_term * alpha if alpha > 0 else 0
+        return entropy, {'entropy_term': entropy_term}
