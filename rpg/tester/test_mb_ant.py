cd48cbb0ee6049eaac2f2fd549ab35cf25823cb2
hzaskywalker <z2huang@eng.ucsd.edu>
2022-11-06 14:48:54 -0800

    update

--- rpg/tester/test_mb_ant.py
+++ rpg/tester/test_mb_ant.py
@@ -2,8 +2,8 @@ import tqdm
 import numpy as np
 from rpg.env_base import GymVecEnv, TorchEnv
 from rpg.ppo import train_ppo
-from rpg.rpgm import Trainer
+from rpg.soft_rpg import Trainer
 
 env = GymVecEnv('Ant-v3', 1, ignore_truncated_done=True)
-trainer = Trainer.parse(env, max_update_step=1000, head=dict(std_mode='statewise', std_scale=1., squash=True), entropy_coef=1., entropy_target=6, actor_optim=dict(max_grad_norm=1.), update_train_step=0, hooks=dict(evaluate_pi=dict()), have_done=True, zero_done_value=False,  horizon=3, weights=dict(done=100.)) # do not know if we need max_grad_norm
+trainer = Trainer.parse(env, head=dict(std_mode='statewise', std_scale=1., squash=True), optim=dict(max_grad_norm=1.), hooks=dict(evaluate_pi=dict()), have_done=True, horizon=3, weights=dict(done=100.)) # do not know if we need max_grad_norm
 trainer.run_rpgm()
