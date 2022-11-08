61bcb4dd1309999857de1cae2b5c915f0359b740
hzaskywalker <z2huang@eng.ucsd.edu>
2022-11-08 15:13:20 -0800

    update

--- rpg/tester/test_mb_hopper.py
+++ rpg/tester/test_mb_hopper.py
@@ -13,6 +13,7 @@ trainer = Trainer.parse(
     wandb=dict(name='hopper'),
     _variants=dict(
         ln=dict(state_layer_norm=True, path='tmp/hopper_ln', weights=dict(state=20000.)),
+        ln2=dict(state_layer_norm=True, path='tmp/hopper_ln', weights=dict(state=1000.)),
     ),
 ) # do not know if we need max_grad_norm
 trainer.run_rpgm()
