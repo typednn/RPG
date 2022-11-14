431dc98afa13ad9a9f8d738bbdbd1504da0636b2
hzaskywalker <z2huang@eng.ucsd.edu>
2022-11-13 23:53:32 -0800

    update

--- envs/maniskill_env.py
+++ envs/maniskill_env.py
@@ -1,5 +1,17 @@
-import mani_skill2.envs
 import gym
 
-def make(env_name):
-    return gym.make(env_name)
+def make():
+    from envs.maniskill.open_carbinet import OpenCabinetDoorEnv
+    pid = '1018'
+    return OpenCabinetDoorEnv(**{'reward_mode': 'dense', 'obs_mode': 'state', 'model_ids': [pid], 'fixed_target_link_idx': 1})
+
+if __name__ == '__main__':
+    env = make()
+    env.reset()
+    images = []
+    images.append(env.render(mode='rgb_array'))
+    for i in range(100):
+        env.step(env.action_space.sample())
+        images.append(env.render(mode='rgb_array'))
+    from tools.utils import animate
+    animate(images, 'output.mp4')
