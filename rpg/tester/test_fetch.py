3d7f3a850b5fb5a3a983969140910292808e2113
hzaskywalker <z2huang@eng.ucsd.edu>
2022-11-15 16:40:24 -0800

    update

--- rpg/tester/test_fetch.py
+++ rpg/tester/test_fetch.py
@@ -43,6 +43,7 @@ trainer = SkillLearning.parse(
     _variants=dict(
         mbrl=dict(z_dim=1),
         maxent=dict(z_dim=0, z_cont_dim=4, ir=dict(reward_decay=dict(init_value=0.2))),
+        maxent2=dict(z_dim=0, z_cont_dim=4, ir=dict(reward_decay=dict(init_value=1.))),
     )
     
 ) # do not know if we need max_grad_norm
