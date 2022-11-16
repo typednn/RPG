1bbb3595d0a12c98fdfc03a5ecd21b1bd7ead75a
hzaskywalker <z2huang@eng.ucsd.edu>
2022-11-16 00:06:10 -0800

    update

--- rpg/tester/test_rpg_cheetah.py
+++ rpg/tester/test_rpg_cheetah.py
@@ -18,6 +18,7 @@ trainer = SkillLearning.parse(
     z_cont_dim=10,
 
     wandb=dict(name='cheetah-rpg'),
+    info=dict(mutual_info_weight=0.1),
     _variants=dict(
     ),
 ) # do not know if we need max_grad_norm
