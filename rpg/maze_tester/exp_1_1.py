13b13c629ca49ab8b178c835b8a9304f7122b1b9
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-01 15:42:06 -0800

    update

--- rpg/maze_tester/exp_1_1.py
+++ rpg/maze_tester/exp_1_1.py
@@ -4,9 +4,11 @@ if __name__ == '__main__':
     exp = build_exp(base_config)
 
     exp.add_exps(
-        'repr',
+        'antcross',
         dict(
             info=dict(coef=[0., 0.05, 0.01]),
             hidden=dict(TYPE=['Categorical', 'Categorical', 'Gaussian'], n=[1, 6, 5]), ), 
         base='antcross', default_env='AntMaze3',
     )
+
+    exp.main()
