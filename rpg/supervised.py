4194fd9a4cf4d0523adc689a2fb77c18798864cd
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-03 00:00:27 -0800

    update

--- rpg/supervised.py
+++ rpg/supervised.py
@@ -1,9 +1,14 @@
+import tqdm
 import matplotlib.pyplot as plt
+import gym
 import torch
 import numpy as np  
+import torch_scatter
+from tools.utils import logger
 from tools.utils import totensor
 from tools.config import Configurable
 from rpg.density import DensityEstimator
+from gym.spaces import Box
 
 """
 # supervised learning for density measurement
@@ -32,7 +37,7 @@ class DatasetBase(Configurable):
         # sample dataset
         raise NotImplementedError
 
-    def count(self, inp):
+    def tokenize(self, inp):
         # return occupancy
         raise NotImplementedError
 
@@ -40,10 +45,16 @@ class DatasetBase(Configurable):
         # visualize the occupancy
         raise NotImplementedError
 
+    def count(self, inp, value, reduce='sum'):
+        index, N = self.tokenize(inp)
+        if value is None:
+            value = torch.ones_like(index) 
+        return torch_scatter.scatter(value, index, dim=0, dim_size=N, reduce=reduce)
+
     def test(self):
         plt.clf()
         data = self.sample(1000)
-        occupancy = self.count(data) 
+        occupancy = self.count(data, None)
         self.visualize(occupancy)
         plt.savefig('test.png')
         
@@ -54,6 +65,9 @@ class GaussianDataset(DatasetBase):
         self.N = N
         self.bins = np.linspace(-5, 5., self.N)
 
+    def get_obs_space(self):
+        return gym.spaces.Box(-5, 5., shape=(1,))
+
     def sample(self, batch_size):
         n1 = int(batch_size * 0.3)
         X = np.concatenate(
@@ -61,12 +75,11 @@ class GaussianDataset(DatasetBase):
         )[:, np.newaxis]
         return totensor(X, device='cuda:0').clip(-5, 4.999999)
 
-    def count(self, inp):
+    def tokenize(self, inp):
         inp = inp.reshape(-1)
         # print(plt.hist(inp.cpu().numpy(), bins=100)[1].shape)
-        inp = ((inp / 10 + 0.5) * self.N).long()
-        count = torch.bincount(inp, minlength=self.N)
-        return count
+        return ((inp / 10 + 0.5) * (self.N-1)).long(), self.N
+
 
     def visualize(self, occupancy):
         #return super().visualize(occupancy)
@@ -85,25 +98,31 @@ class Env2DDataset(DatasetBase):
         self.xedges = np.linspace(0., 1, self.N + 1)
         self.yedges = np.linspace(0., 1, self.N + 1)
 
+    def get_obs_space(self):
+        return gym.spaces.Box(-5, 5., shape=(3,))
+
     def sample(self, batch_size):
         idx = np.random.choice(self.data.shape[0], batch_size)
         return totensor(self.data[idx], device='cuda:0')
 
-    def count(self, inp):
-        inp = inp[..., :2].reshape(-1, 2).detach().cpu().numpy()
+    def tokenize(self, inp):
+        inp = inp[..., :2].reshape(-1, 2) #.detach().cpu().numpy()
         # plt.scatter(inp[:, 0], inp[:, 1])
         # plt.savefig('test.png')
         # exit(0)
-        count, xedges, yedges = np.histogram2d(
-            inp[:, 1], inp[:, 0], bins=self.N, range=[[0., 1], [0., 1]]
-        )
-        #plt.hist2d(inp[:, 0], inp[:, 1], bins=self.N, range=[[-1., 1], [-1., 1]])
-        return totensor(count, device='cuda:0')
+        # count, xedges, yedges = np.histogram2d(
+        #     inp[:, 1], inp[:, 0], bins=self.N, range=[[0., 1], [0., 1]]
+        # )
+        # #plt.hist2d(inp[:, 0], inp[:, 1], bins=self.N, range=[[-1., 1], [-1., 1]])
+        # return totensor(count, device='cuda:0')
+        x = (inp[:, 1] * (self.N-1)).long()
+        y = (inp[:, 0] * (self.N-1)).long()
+        return x * self.N + y, self.N * self.N
 
     def visualize(self, occupancy):
-        count2d = occupancy.detach().cpu().numpy()
+        count2d = occupancy.detach().cpu().numpy().reshape(self.N, self.N)
         plt.pcolormesh(self.xedges, self.yedges, count2d, shading='auto')
-        pass
+        plt.colorbar()
 
 
 def make_dataset(dataset_name):
@@ -117,15 +136,46 @@ def make_dataset(dataset_name):
 
 
 class Trainer(Configurable): 
-    def __init__(self, cfg=None, dataset_name=None, 
-                 density=DensityEstimator.to_build("RND")) -> None:
+    def __init__(
+        self, cfg=None, dataset_name=None, 
+        density=DensityEstimator.to_build(TYPE="RND"),
+        max_epoch=1000,
+        batch_size = 256,
+        batch_num=2000,
+        path = None
+    ) -> None:
         super().__init__()
         self.dataset = make_dataset(dataset_name)
-        self.density = DensityEstimator.to_build(self.dataset.get_obs_space(), cfg=density)
+        self.density: DensityEstimator = DensityEstimator.build(self.dataset.get_obs_space(), cfg=density).cuda()
+
+        self.max_epoch = max_epoch
+        self.batch_size = batch_size
+        self.batch_num = batch_num
+
+        # TODO make logger with wandb easier to config ..
+        logger.configure(dir=path, format_strs=['stdout', 'log', 'csv'])
 
     def test_dataset(self):
         self.dataset.test()
 
+    def train(self):
+        for i in tqdm.trange(self.max_epoch):
+            for j in tqdm.trange(self.batch_num):
+                data = self.dataset.sample(self.batch_size)
+                self.density.update(data)
+
+            with torch.no_grad():
+                data = self.dataset.sample(1000)
+                log_prob = self.density.log_prob(data)
+                value = self.dataset.count(data, log_prob[..., 0], reduce='mean')
+
+
+                plt.clf()
+                self.dataset.visualize(value)
+                logger.savefig('log_prob_avg.png')
+
+
 if __name__ == '__main__':
     trainer = Trainer.parse(dataset_name='twonormal')
-    trainer.test_dataset()
+    #trainer.test_dataset()
+    trainer.train()
