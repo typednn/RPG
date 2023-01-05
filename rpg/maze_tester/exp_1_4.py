f94851d19ea440e66b3f3db323efc64185f3e909
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-04 22:25:48 -0800

    update

--- rpg/maze_tester/exp_1_4.py
+++ rpg/maze_tester/exp_1_4.py
@@ -67,15 +67,17 @@ if __name__ == '__main__':
     exp.add_exps(
         'blocktry',
         dict(
+            save_video=300,
             steps_per_epoch=120, 
+            hooks=dict(save_traj=dict(n_epoch=20)),
+            env_cfg=dict(
+                n_block=[1, 1, 2, 2, 3, 3, 1, 2, 3]
+            ),
             hidden=dict(
                 TYPE='Categorical',
-                n=[12, 1, 12, 1, 12, 1]
+                n=[12, 1, 12, 1, 12, 1, 6, 6, 6]
             ),
-            env_cfg=dict(
-                n_block=[1, 1, 2, 2, 3, 3]
-            ),
-            info=dict(coef=0.05),
+            info=dict(coef=0.01),
         ),
         base='block', default_env='BlockPush',
     )
