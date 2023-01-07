93f9578f7b15ec68ec111ee971393e69316b2865
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-06 16:38:59 -0800

    update

--- rpg/maze_tester/exp_1_6.py
+++ rpg/maze_tester/exp_1_6.py
@@ -4,27 +4,7 @@ if __name__ == '__main__':
     exp = build_exp(base_config)
 
     exp.add_exps(
-        'small3',
-        dict(
-            reward_scale=0.,
-            env_cfg=dict(n=1, action_scale=0.3),
-            max_epoch=100,
-            rnd=dict(
-                density=dict(
-                    TYPE='RND',
-                    normalizer='ema',
-                ),
-            ),
-            hidden=dict(n=[1, 1, 6, 6]),
-            pi_a=dict(ent=dict(coef=[0.01, 0.001, 0.01, 0.001])),
-            head=dict(std_scale=0.4, std_mode='statewise'),
-        ),
-        base='small', default_env='SmallMaze',
-        names=['rl', 'rl001', 'rpg', 'rpg001']
-    )
-
-    exp.add_exps(
-        'antcross4',
+        'antfork',
         dict(
             reward_scale=0.,
             rnd=dict(
@@ -36,30 +16,30 @@ if __name__ == '__main__':
             ),
             hidden=dict(
                 TYPE='Categorical',
-                n=[12, 12]
+                n=[12, 1]
             ),
-            info=dict(coef=[0.05, 0.1]),
+            info=dict(coef=[0.03, 0.0]),
         ),
-        base='antcross', default_env='AntMaze3',
-        names=['rnd12_5', 'rnd12_10']
+        base='antcross', default_env='AntFork',
+        names=['rnd', 'rl']
     )
 
-    exp.add_exps(
-        'blocktry',
-        dict(
-            save_video=300,
-            steps_per_epoch=120, 
-            hooks=dict(save_traj=dict(n_epoch=20)),
-            env_cfg=dict(
-                n_block=[1, 1, 2, 2, 3, 3, 1, 2, 3]
-            ),
-            hidden=dict(
-                TYPE='Categorical',
-                n=[12, 1, 12, 1, 12, 1, 6, 6, 6]
-            ),
-            info=dict(coef=0.01),
-        ),
-        base='block', default_env='BlockPush',
-    )
+    # exp.add_exps(
+    #     'blocktry',
+    #     dict(
+    #         save_video=300,
+    #         steps_per_epoch=120, 
+    #         hooks=dict(save_traj=dict(n_epoch=20)),
+    #         env_cfg=dict(
+    #             n_block=[1, 1, 2, 2, 3, 3, 1, 2, 3]
+    #         ),
+    #         hidden=dict(
+    #             TYPE='Categorical',
+    #             n=[12, 1, 12, 1, 12, 1, 6, 6, 6]
+    #         ),
+    #         info=dict(coef=0.01),
+    #     ),
+    #     base='block', default_env='BlockPush',
+    # )
 
     exp.main()
