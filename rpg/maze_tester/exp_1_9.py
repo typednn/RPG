5d8acbdde62ccca058e6a314b251dd2850f09c5b
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-10 00:45:30 -0800

    update

--- rpg/maze_tester/exp_1_9.py
+++ rpg/maze_tester/exp_1_9.py
@@ -6,24 +6,23 @@ if __name__ == '__main__':
     exp.add_exps(
         'eearm',
         dict(
-            reward_scale=0.,
-            rnd=dict(
-                density=dict(
-                    TYPE='RND',
-                    normalizer='ema',
-                ),
-                scale = 0.1,
-            ),
+            # reward_scale=0.,
+            # rnd=dict(
+            #     density=dict(
+            #         TYPE='RND',
+            #         normalizer='ema',
+            #     ),
+            #     scale = 0.1,
+            # ),
             hidden=dict(
                 TYPE=['Categorical'] * 6 + ['Gaussian'] * 6,
                 n=[12, 1, 12, 12, 12, 12] + [12] * 6
             ),
             info=dict(coef=[0.1, 0.0, 0.05, 0.01, 0.001, 0.005, 0.0005, 0.005, 0.001, 0.01, 0.0001, 0.1]),
-            env_cfg=dict(n=[5, 5, 5, 5, 5, 5] + [1] * 6),
-            #save_video=100,
+            env_cfg=dict(n=5),
         ),
-        base='antcross', default_env='AntFork',
-        names=['rnd', 'rl', 'rndx5', 'rnd001', 'rnd0005', 'rnd01'] + ['g0005', 'g005', 'g001', 'g01', 'g05', 'g1']
+        base='eearm', default_env='EEArm',
+        #names=['rnd', 'rl', 'rndx5', 'rnd001', 'rnd0005', 'rnd01'] + ['g0005', 'g005', 'g001', 'g01', 'g05', 'g1']
     )
 
 
