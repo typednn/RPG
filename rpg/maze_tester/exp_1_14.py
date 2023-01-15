0134fa88f19b5dd27e0a966c7123113ac9cb722c
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-15 01:45:45 -0800

    update

--- rpg/maze_tester/exp_1_14.py
+++ rpg/maze_tester/exp_1_14.py
@@ -3,16 +3,16 @@ from rpg.maze_tester.maze_exp import *
 if __name__ == '__main__':
     exp = build_exp(base_config)
 
-    exp.add_exps(
-        'kitcheninfo',
-        dict(
-            #_base=['rpgcv2', 'mbsacv3'],
-            env_cfg=dict(reward_type='bonus', n=5),
-            buffer=dict(max_episode_num=5000),
-            info=dict(coef=[0.002, 0.005, 0.01, 0.0005, 0.0001, 0.0008])
-        ),
-        base='rpgcv2', default_env='Kitchen',
-    )
+    # exp.add_exps(
+    #     'kitcheninfo',
+    #     dict(
+    #         #_base=['rpgcv2', 'mbsacv3'],
+    #         env_cfg=dict(reward_type='bonus', n=5),
+    #         buffer=dict(max_episode_num=5000),
+    #         info=dict(coef=[0.002, 0.005, 0.01, 0.0005, 0.0001, 0.0008])
+    #     ),
+    #     base='rpgcv2', default_env='Kitchen',
+    # )
 
     exp.add_exps(
         'kitchenreward',
