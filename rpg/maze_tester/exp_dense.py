af6c339d014419083b48bb653bcfbd553af66d5c
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-17 01:17:19 -0800

    update

--- rpg/maze_tester/exp_dense.py
+++ rpg/maze_tester/exp_dense.py
@@ -15,14 +15,14 @@ if __name__ == '__main__':
             f'dense{env_name}',
             dict(
                 env_cfg=dict(n=5, reward_type='dense', obs_dim=0),
-                _base=['mbsacv3', 'rpgdv3', 'rpgdv3', 'rpgdv3', 'rpgdv3', 'rpgdv3'],
-                info=dict(coef=[0.0, 0.5, 1., 5., 10., 50.]),
+                _base=['mbsacv3', 'rpgdv3', 'rpgdv3', 'rpgdv3', 'rpgdv3', 'rpgdv3', 'rpgcv2'],
+                info=dict(coef=[0.0, 0.5, 1., 5., 10., 50., 1.]),
                 rnd=dict(scale=0.),
                 z_delay=4,
                 pi_a=dict(ent=dict(coef=1.)),
                 pi_z=dict(ent=dict(coef=100.)),
             ),
-            names=['sac', 'discrete5', 'discrete10', 'discrete50', 'discrete100', 'discrete500'],
+            names=['sac', 'discrete5', 'discrete10', 'discrete50', 'discrete100', 'discrete500', 'gaussian10'],
             base='mbsacv3', 
             default_env = configs['env_name'][env_name],
         )
