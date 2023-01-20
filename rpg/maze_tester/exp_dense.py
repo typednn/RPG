5ea28063e3384aa2315fc5d26d58f816c978cb77
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-20 01:17:52 -0800

    update

--- rpg/maze_tester/exp_dense.py
+++ rpg/maze_tester/exp_dense.py
@@ -12,24 +12,28 @@ if __name__ == '__main__':
     exp = build_exp(base_config)
 
     for env_name  in ['cabinet', 'ant', 'fall']: # ensure the experiments are finished ..
+
+        schedule = "1000000" if env_name == 'ant' else "800000"
         exp.add_exps(
             f'dense{env_name}',
             dict(
-                env_cfg=dict(n=5, reward_type='dense', obs_dim=0),
+                env_cfg=dict(n=[5] * 9 + [1], reward_type='dense', obs_dim=0),
                 _base=[
                     'mbsacv3', 'rpgdv3', 'rpgdv3', 'rpgdv3', 'rpgdv3', 
-                    'rpgdv3', 'rpgcv2', 'rewardrpg', 'rewardrpg'],
+                    'rpgdv3', 'rpgcv2', 'rewardrpg', 'rewardrpg', 'rewardrpg',
+                    ],
                 info=dict(coef=[0.0, 0.5, 1., 5., 10., 
-                                50., 1., 1., 1.]),
+                                50., 1., 1., 1., 1.,]),
                 rnd=dict(scale=0.),
                 z_delay=4,
                 pi_a=dict(ent=dict(coef=1.)),
                 pi_z=dict(ent=dict(coef=100.)),
-                reward_schedule=["1000000" if env_name == 'ant' else "800000"] * 8 + ['2seg(0.25,240000,500000)'],
+                reward_schedule=["1000000" if env_name == 'ant' else "800000"] * 8 + [
+                    '2seg(0.25,240000,500000)', schedule],
             ),
             names=[
                 'sac', 'discrete5', 'discrete10', 'discrete50', 'discrete100',
-                'discrete500', 'gaussian10', 'incrR', 'seg'], # note that seg is seg2 before
+                'discrete500', 'gaussian10', 'incrR', 'seg', 'incrRn1'], # note that seg is seg2 before
             base='mbsacv3', 
             default_env = configs['env_name'][env_name],
         )
