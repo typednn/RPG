b3add72909f2d43bf7a6582e45fe668d741e90ed
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-23 09:29:27 -0800

    update

--- rpg/maze_tester/exp_denseant.py
+++ rpg/maze_tester/exp_denseant.py
@@ -45,7 +45,13 @@ if __name__ == '__main__':
                     env_cfg=dict(n=1),
                     reward_schedule='2seg(0.1,400000,600000)',
                     trainer=dict(gamma=0.995),
-                )
+                ),
+                sacn1gamma=dict(
+                    _base='mbsacv3',
+                    env_cfg=dict(n=1),
+                    reward_schedule=None,
+                    trainer=dict(gamma=0.995),
+                ),
             )
         ),
         base=None, 
