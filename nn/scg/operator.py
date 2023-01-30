d65df93b1e2055b0349c35d58559baa176a87c64
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-30 14:59:52 -0800

    update

--- nn/scg/operator.py
+++ nn/scg/operator.py
@@ -1,4 +1,6 @@
+import typing
 from torch.nn import Module
+from .basetypes import Arrow
 # pytorch module, but typed
 # shapes that we support:
 #   - (num1, num2, num3)
@@ -11,25 +13,36 @@ from torch.nn import Module
 
 # operator can take multiple types .. but during type inference, the results must be the same ..
 
+def get_type_from_op_or_type(op_or_type):
+    if isinstance(op_or_type, Operator):
+        return op_or_type.out
+    else:
+        return op_or_type
 
 
 class Operator(Module):
-    Arrow = None # TYPE annotation of the forward funciton
+    arrow: typing.Optional[Arrow] = None # TYPE annotation of the forward funciton
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__()
 
     def register_types(self, *args, **kwargs):
-        pass
+        assert len(kwargs) == 0, NotImplementedError
+        if self.arrow is not None:
+            self._inp_type, _, self._oup_type = self.arrow.unify(
+                *map(get_type_from_op_or_type, args)
+            )
 
     @property
     def out(self): # out type when input are feed ..
-        pass
+        return self._oup_type
 
     def _get_type(self):
         if not hasattr(self, '_type'):
             self._type = self.get_type()
         return self._type
 
-    def get_type(self):
-        raise NotImplementedError
+
+    def __str__(self) -> str:
+        out = super().__str__()
+        return out + f" {self.arrow}: {self.out}"
