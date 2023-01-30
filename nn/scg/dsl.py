11d219889795d19415b213c1035cfc1596b6b3c6
hzaskywalker <z2huang@eng.ucsd.edu>
2023-01-30 15:25:40 -0800

    update

--- nn/scg/dsl.py
+++
@@ -1,3 +0,0 @@
-# implementation of basic operators
-#   include contact, batchify, type conversion
-#   we implement abstract methods, for which one can either (use the default config, parse the config to build a new one, or use a shared config)
